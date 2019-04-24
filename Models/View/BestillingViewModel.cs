3d0887792363fa5b5341f1c1e42a5dc83f1030ad
ubihhm <48522269+ubihhm@users.noreply.github.com>
2019-04-24 23:19:00 +0200

    Add files via upload

---
+++ Models/View/BestillingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza.Models.View
+{
+    public class BestillingViewModel
+    {
+      
+        [Required(ErrorMessage = "Brugernavn skal udfyldes")]
+        public string Brugernavn { get; set; }
+        [Display(Name = "Pizza Margherita")]
+        [Range(0, 9, ErrorMessage = "Bestil et rimeligt antal (0-9)")]
+        public int AntalMargherita { get; set; }
+        [Display(Name = "Pizza Capricciosa")]
+        [Range(0, 9, ErrorMessage = "Bestil et rimeligt antal (0-9)")]
+        public int AntalCapricciosa { get; set; }
+        [Display(Name = "Pizza Quattro Stagioni")]
+        [Range(0, 9, ErrorMessage = "Bestil et rimeligt antal (0-9)")]
+        public int AntalQuattroStagioni { get; set; }
+        public string Meddelelse { get; set; }
+    }
+}
+
